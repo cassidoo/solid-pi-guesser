79b1570d541784454ea9b7304a7a5d38f11e9e9e
Cassidy Williams <cassidoo@users.noreply.github.com>
2022-03-10 16:49:48 -0600

    Update endgame

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,32 @@ function App() {
   const [numberOfLives, setNumberOfLives] = createSignal(3);
   const digitsGuessed = createSignal(0);
 
+  let tweetString = `I just guessed ${digitsGuessed[0]()} digits of pi!`;
+  const website = `https://github.com/cassidoo/solid-pi-guesser`;
+
   return (
     <div class={styles.App}>
-      What's the next digit, champ?
-      <InputComponent numLives={setNumberOfLives} numDigits={digitsGuessed} />
+      {numberOfLives() > 0 && (
+        <>
+          What's the next digit, champ?
+          <InputComponent
+            numLives={setNumberOfLives}
+            numDigits={digitsGuessed}
+          />
+        </>
+      )}
       <Pi digits={digitsGuessed[0]} />
-      <div>Number of lives: {numberOfLives()}</div>
+      {numberOfLives() > 0 && <div>Number of lives: {numberOfLives()}</div>}
+      {numberOfLives() === 0 && (
+        <>
+          <h2>You correctly guessed {digitsGuessed[0]()} digits of pi!</h2>
+          <a
+            href={`https://twitter.com/intent/tweet?text=${tweetString}&url=${website}&via=cassidoo`}
+          >
+            Tweet your score!
+          </a>
+        </>
+      )}
     </div>
   );
 }
